34fd58f2498c30cc6fdef5c8b7904ac234b2fa83
Amey Manjrekar <ameymanjrekar225371@gmail.com>
2022-02-24 21:48:50 +0530

    Removing keys

--- BikeSharing/src/main/scala/com/capital/bikesharing/utils/Constant.scala
+++ BikeSharing/src/main/scala/com/capital/bikesharing/utils/Constant.scala
@@ -1,8 +1,8 @@
 package com.capital.bikesharing.utils
 
 object Constant {
-  val AWS_ACCESS_KEY = "AKIAZCTNU5SVRRQ4S3GJ"
-  val AWS_SECRET_KEY = "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"
+  val AWS_ACCESS_KEY = ""
+  val AWS_SECRET_KEY = ""
   val BUCKET_NAME="haqbikesharing"
   val FTP_PATH="https://s3.amazonaws.com/capitalbikeshare-data/"
 }
